57ed7bad3a70a97024bcc1974d01e4824bb41ef8
jake-freeman <jfreeman@hawk.iit.edu>
2015-08-31 21:22:45 -0500

    Added basic layout and components + some logic

--- STFrame.java
+++ STFrame.java
@@ -8,17 +8,77 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.util.*;
 import java.io.*;
 
 public class STFrame extends JFrame
 {
+    private JButton[] colors1;
+    private JButton[] colors2;
+
+    private JComboBox p1;
+    private JComboBox p2;
+
     public STFrame()
     {
         super("Stream Thing");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
 
+        ArrayList<File>   fileList = getFileList("chars");
+        ArrayList<String> charList = getCharList(fileList);
+
+        colors1 = new JButton[6];
+        colors2 = new JButton[6];
+        p1      = new JComboBox(charList.toArray());
+        p2      = new JComboBox(charList.toArray());
+
+        addActionListeners();
+        addComponentsToPane(getContentPane());
+
+        pack();
         setVisible(true);
+    }
+
+    /**
+    * Adds all required action listeners to combo boxes
+    * and buttons.
+    */
+    private void addActionListeners()
+    {
+        p1.addActionListener
+        (
+            new ActionListener()
+            {
+                public void actionPerformed(ActionEvent e)
+                {
+                    System.out.println("Selected: " + (String)((JComboBox)e.getSource()).getSelectedItem());
+                    charSelect((String)((JComboBox)e.getSource()).getSelectedItem());
+                }
+            }
+        );
+    }
+
+    /**
+    * Function to add components to the main content area.
+    *
+    * @param pane the content area to add UI items to
+    * 
+    */
+    private void addComponentsToPane(Container pane)
+    {
+        pane.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.insets = new Insets(20,10,20,10);  // padding
+        c.gridx = 0;
+        c.gridy = 0;
+        pane.add(p1, c);
+
+        c.gridx = 6;
+        pane.add(p2, c);
     }
 
     /**
@@ -33,11 +93,39 @@ public class STFrame extends JFrame
         ArrayList<String> chars = new ArrayList<String>();
         for (File f: files)
         {
-            // [HACK]: Files should be named differently
             String character = f.getName().replace(".png", "");
-            if (!Character.isUpperCase(character.charAt(character.length() - 1)))
+            // this effectively picks the default character file for a name
+            if (character.charAt(character.length() - 1) == 'A')
+            {
+                // omits 'A' at the begining of the string
+                character = character.substring(0, character.length() - 1);
+                // quick and dirty translation table for longer character names
+                switch (character)
+                {
+                    case "Captain":
+                        character = "Captain-Falcon";
+                        break;
+                    case "Donkey":
+                        character = "Donkey-Kong";
+                        break;
+                    case "Dr":
+                        character = "Dr-Mario";
+                        break;
+                    case "Ice":
+                        character = "Ice-Climbers";
+                        break;
+                    case "Mr":
+                        character = "Mr-Game-and-Watch";
+                        break;
+                    case "Young":
+                        character = "Young-Link";
+                        break;
+                }
                 chars.add(character);
+            }
         }
+        // sorts by numerical value
+        Collections.sort(chars);
         return chars;
     }
 
